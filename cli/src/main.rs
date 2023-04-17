2b8dc455d84a44c05e151d17e3dffdb6f3694672
Bruno Tavares <connect+github@bltavares.com>
2023-04-17 12:49:35 -0300

    Fix fmt

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -13,17 +13,19 @@ fn file_name(f: PathBuf) -> String {
     f.display().to_string().to_lowercase()
 }
 
-
 #[cfg_attr(not(target_os = "wasi"), tokio::main)]
 #[cfg_attr(target_os = "wasi", tokio::main(flavor = "current_thread"))]
 async fn main() {
-    let name = std::env::args().nth(1).expect("No binary name given as first argument.");
+    let name = std::env::args()
+        .nth(1)
+        .expect("No binary name given as first argument.");
     let regex = Regex::new(&name).expect("not a valid regex pattern.");
 
     let found = async_which::which_re(regex)
         .map_ok(file_name)
         .try_collect::<HashSet<_>>()
-        .await.expect("failed to find binary");
+        .await
+        .expect("failed to find binary");
 
     if found.is_empty() {
         println!("Not found: {}", name);
